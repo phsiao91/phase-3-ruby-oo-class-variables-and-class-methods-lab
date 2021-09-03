09ee337c381b4e3bb2164f1d5d8d27a35cb94e0a
Phing An Hsiao <phinganhsiao@gmail.com>
2021-09-03 00:10:01 -0400

    commit tree f37a946771dd5e6f5edcc22c1367e689975844ed

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genra = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+end
